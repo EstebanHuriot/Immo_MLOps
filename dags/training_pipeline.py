1b2d94361cc711fdfd77d81b151d9b451ef2372e
EstebanHuriot <esteban.martin@bsb-education.com>
2026-07-08 20:57:05 +0200

    Work on training pipeline dag

--- dags/training_pipeline.py
+++ dags/training_pipeline.py
@@ -1,7 +1,6 @@
 from datetime import datetime, timedelta
 from airflow.decorators import dag, task
 from airflow import DAG
-from airflow.utils.dates import days_ago
 import subprocess
 import sys
 
@@ -10,18 +9,28 @@ import sys
 def collect():
     subprocess.run([sys.executable, "../src/script.py"], check=True)
 
+@task
+def preprocess():
+    subprocess.run([sys.executable, "../src/process.py"], check=True)
 
-
+@task
+def train():
+    subprocess.run([sys.executable, "../src/train.py"], check=True)
 
 
 @dag(
     dag_id='model_dag',
     tags=['ImmoMLOps', 'datascientest'],
     schedule_interval=None,
-    start_date=days_ago(0)
+    start_date=datetime(2026, 7, 8), # day I wrote it
+    catchup=False
     )
 
-def my_dag():
-    my_task1 = collect()
+def model_dag():
+    collect_task = collect()
+    process_task = preprocess()
+    train_task = train()
 
-my_dag = my_dag()
+    collect_task >> process_task >> train_task
+
+dag = model_dag()
